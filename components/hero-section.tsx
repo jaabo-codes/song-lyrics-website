c8a14152a63aee24326ca606ebb0117ec4fc890a
jaabo-codes <nabi.mustajab@gmail.com>
2025-12-12 23:44:55 +0500

    Changed the Homepage

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,17 +1,31 @@
-import Laptop from "@/public/laptop.png";
+import Image from "next/image";
 export function HeroSection() {
   return (
-    <section className="relative py-16 pb-32">
+    <section className="relative pt-16 ">
       <div className="container mx-auto px-6">
         <h1 className="text-4xl font-bold">Discover Song Lyrics</h1>
-        <div className="bg-black text-white p-20 rounded-3xl dark:bg-white dark:text-black mt-6 flex justify-between items-center">
+        <div
+          className="bg-gradient-to-r from-pink-500 via-purple-600 to-indigo-700 text-white px-20 rounded-3xl 
+          mt-6 flex justify-between items-center h-[300px]"
+        >
           <div>
-          <h2 className="text-3xl font-bold">The Worlds Largest Collection <br /> of Song Lyrics</h2>
-          <p className="text-lg mt-4">Explore millions of song lyrics from your favorite artists</p>
+            <h2 className="text-3xl font-bold">
+              The Worlds Largest Collection <br /> of Song Lyrics
+            </h2>
+            <p className="text-lg mt-4">
+              Explore millions of song lyrics from your favorite artists
+            </p>
           </div>
-          <img src="./laptop.png" alt="" className="w-1/2" />
+
+          <Image
+            src="/images/music.png"
+            alt="music"
+            width={20}
+            height={20}
+            className="w-100 h-75"
+          />
         </div>
       </div>
     </section>
-  )
+  );
 }
